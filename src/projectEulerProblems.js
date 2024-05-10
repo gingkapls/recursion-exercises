e3030cf1fc6a027a9f46c0aac603c7352428ccc0
gin <73906888+gingkapls@users.noreply.github.com>
2024-05-10 14:01:56 +0530

    add fibs and listPrimeFactor functions

--- src/projectEulerProblems.js
+++ src/projectEulerProblems.js
@@ -8,3 +8,37 @@ const sumOfMultiples = (x) =>
 
 console.log(sumOfMultiples(999));
 
+// 2. sum of even fibonacci
+const fibs = (x) => {
+  if (x === 0) return [];
+  if (x === 1) return [0];
+  if (x === 2) return [0, 1];
+
+  const seq = fibs(x - 1);
+
+  return seq.concat(seq.at(-1) + seq.at(-2));
+};
+
+console.log(fibs(50).reduce((sum, el) => ((el & 1) === 0 ? sum + el : sum)));
+
+// 3. List prime factors 
+const listPrimeFactors = (x, res = []) => {
+  if (x <= 1) return [];
+
+  let i = 2;
+  for (; i <= x; ++i) {
+    if (x % i === 0) {
+      res.push(i);
+      break;
+    }
+    if (i === x) res.push(i);
+  }
+
+  // re-order for TCO
+  listPrimeFactors(x / i, res);
+  return res;
+};
+
+const num = 600_851_475_143;
+console.log("num: ", num);
+console.log(listPrimeFactors(num));
