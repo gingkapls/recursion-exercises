f917dbb7e61c795e7590f4fa404faa41c9d1521a
gin <73906888+gingkapls@users.noreply.github.com>
2024-05-10 14:00:21 +0530

    rewrite it to not be terrible

--- src/fib.js
+++ src/fib.js
@@ -1,27 +1,26 @@
-// Functional approach
-const fibF = (x) =>
-  x <= 0
-    ? [0]
-    : x <= 2
-      ? [0, 1]
-      : fibF(x - 1).concat([fibF(x - 1).at(-2) + fibF(x - 1).at(-1)]);
+// Recurisve approach
+const fibsRec = (x) => {
+  if (x === 0) return [];
+  if (x === 1) return [0];
+  if (x === 2) return [0, 1];
 
+  const seq = fibsRec(x - 1);
+  return seq.concat(seq.at(-1) + seq.at(-2));
+};
 
 // Iterative
 
 const fibs = (x) => {
-    let res = [0, 1];
-    if (x <= 0) return [];
-    if (x <= 2) return res.toSpliced(x);
-    
-    for (let i = 2; i < x; ++i) {
-        res.push(res.at(-1) + res.at(-2)); 
-    }
-    
-    return res;
-}
+  let res = [0, 1];
+  if (x <= 0) return [];
+  if (x <= 2) return res.toSpliced(x);
 
+  for (let i = 2; i < x; ++i) {
+    res.push(res.at(-1) + res.at(-2));
+  }
 
+  return res;
+};
 
 console.log(fibs(8));
-console.log(fibF(8));
+console.log(fibsRec(8));
