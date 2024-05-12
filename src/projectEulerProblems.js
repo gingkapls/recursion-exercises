e58b4b56d4bdd4abe629c9b43eb1346dc1fbd568
gin <73906888+gingkapls@users.noreply.github.com>
2024-05-12 14:50:24 +0530

    projectEuler: add functional fib function, add memoize function

--- src/projectEulerProblems.js
+++ src/projectEulerProblems.js
@@ -21,7 +21,7 @@ const fibs = (x) => {
 
 console.log(fibs(50).reduce((sum, el) => ((el & 1) === 0 ? sum + el : sum)));
 
-// 3. List prime factors 
+// 3. List prime factors
 const listPrimeFactors = (x, res = []) => {
   if (x <= 1) return [];
 
@@ -39,6 +39,48 @@ const listPrimeFactors = (x, res = []) => {
   return res;
 };
 
+// sumAll
+
+const sumAllSmart = (arr1, arr2) => {
+  const n1 = arr1.reduce((total, num) => total + num);
+  const n2 = arr2.reduce((total, num) => total + num);
+
+  return n1 * n2;
+};
+
+console.log("sumAll: ", sumAllSmart([1, 2], [5, 6]));
+
 const num = 600_851_475_143;
 console.log("num: ", num);
 console.log(listPrimeFactors(num));
+
+// Memoize so our functional fib can actually calculate more than 15 terms
+const memoizeFunc =
+  (fn) =>
+  function memoFn(arg) {
+    if (memoFn?.values === undefined) {
+      memoFn.values = {};
+    }
+
+    if (memoFn.values?.[arg] === undefined) {
+      memoFn.values[arg] = fn(arg);
+    }
+
+    return memoFn.values[arg];
+  };
+
+
+// Functional approach
+let fibF = (x) =>
+  x <= 0
+    ? []
+    : x === 1
+      ? [0]
+      : x === 2
+        ? [0, 1]
+        : fibF(x - 1).concat(fibF(x - 1).at(-1) + fibF(x - 1).at(-2));
+
+fibF = memoizeFunc(fibF);
+
+console.log(fibF(10));
+console.log(fibF.values);
